0e2b91c821b35c1a60ba77e4ee4903b16b483e93
Mr Vlad <vlad.parakhin+atko@okta.com>
2025-03-24 12:48:20 +0100

    update

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the environment variable for BigQuery service account authentication
-export GOOGLE_APPLICATION_CREDENTIALS="/path/to/your/service-account-key.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/vlad/okta/okta-ga-rollup-83c0374865e4.json"
 
 # Verify the variable is set
 echo "Service account path is set to: $GOOGLE_APPLICATION_CREDENTIALS"
